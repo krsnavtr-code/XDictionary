772c2916084c2ce12b76dfc25ab1a1eb32a5359c
Krishna Avtar <113005173+krsnavtr-code@users.noreply.github.com>
2024-03-03 17:20:41 +0530

    handle search with jsx

--- src/container/XDictionary.jsx
+++ src/container/XDictionary.jsx
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
-const XDictionary = () => {
-  // Initialize dictionary state
-  const initialDictionary = [
+function XDictionary() {
+  const [search, setSearch] = useState("");
+  const [meaning, setMeaning] = useState("");
+  const dictionary = [
     {
       word: "React",
       meaning: "A JavaScript library for building user interfaces.",
@@ -11,51 +12,34 @@ const XDictionary = () => {
     { word: "State", meaning: "An object that stores data for a component." },
   ];
 
-  // State for the search term and result message
-  const [searchTerm, setSearchTerm] = useState("");
-  const [resultMessage, setResultMessage] = useState("");
-
-  // Function to handle search
   const handleSearch = () => {
-    const lowerCaseSearchTerm = searchTerm.toLowerCase();
-    const foundWord = initialDictionary.find(
-      (entry) => entry.word.toLowerCase() === lowerCaseSearchTerm
+    const wordMeaning = dictionary.find(
+      (entry) => entry.word.toLowerCase() === search.toLowerCase()
     );
-
-    if (foundWord) {
-      setResultMessage(`Definition: ${foundWord.meaning}`);
-    } else {
-      setResultMessage("Word not found in the dictionary.");
-    }
-  };
-
-  // Function to handle button click
-  const handleButtonClick = () => {
-    handleSearch(); 
+    const foundMeaning = wordMeaning
+      ? wordMeaning.meaning
+      : "Word not found in the dictionary.";
+    setMeaning(foundMeaning);
   };
 
   return (
     <div>
       <h1>Dictionary App</h1>
-      <label htmlFor="searchInput"></label>
-      <input
-        type="text"
-        id="searchInput"
-        value={searchTerm}
-        placeholder="Search for a word..."
-        onChange={(e) => setSearchTerm(e.target.value)}
-      />
-      <button onClick={handleButtonClick}>Search</button>
       <div>
-        {resultMessage && (
-          <p>
-            <strong>{resultMessage}</strong>
-          </p>
-        )}
+        <input
+          type="text"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          placeholder="Search for word..."
+        />
+        <button onClick={handleSearch}>Search</button>
+      </div>
+      <div>
+        <strong>Definition:</strong>
+        <div>{meaning}</div>
       </div>
     </div>
-    
   );
-};
+}
 
 export default XDictionary;
